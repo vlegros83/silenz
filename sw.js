49219b7469d03bbaf313041ee6762aa9b7150221
vlegros83 <vlegros@gmail.com>
2026-07-07 08:29:22 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,14 @@
-const CACHE = 'silenz-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE = 'silenz-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './nlms-worklet.js'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+});
+self.addEventListener('fetch', e =>
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
+);
